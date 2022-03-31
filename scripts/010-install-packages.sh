842a170de30cdf78940eac4e5d83b7ca1da768e0
Juan Pablo Noreña <jpnorenam@unal.edu.co>
2022-03-30 21:36:49 -0500

    updates maas snap channel

--- scripts/010-install-packages.sh
+++ scripts/010-install-packages.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Installing packages..."
 snap install jq
-snap install maas --channel latest/stable
+sudo snap install --channel=3.1/edge maas
 
 # Install PostgreSQL for MAAS
 apt-get update --yes
